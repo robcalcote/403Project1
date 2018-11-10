4dc41344f06df84af44d654442e16b1638e5e74f
Rob <robcalcote@gmail.com>
2018-11-09 17:38:29 -0700

    Added Schools controller and View w/ dropdown and button to submit

---
+++ 403Project1/Controllers/SchoolsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _403Project1.Controllers
+{
+    public class SchoolsController : Controller
+    {
+        // GET: Schools
+        public ActionResult Index()
+        {
+            var schoolsList = new SelectList(new[]
+            {
+                new { ID = "1", Name = "Orem" },
+                new { ID = "2", Name = "Riverton" },
+                new { ID = "3", Name = "Gilbert" },
+            }, "ID", "Name", 1);
+
+            ViewData["list"] = schoolsList;
+
+
+            // returns view
+            return View();
+        }
+    }
+}
