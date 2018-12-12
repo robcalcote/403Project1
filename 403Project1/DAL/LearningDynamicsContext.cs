3541a641b095d965b543861bca01862c92b9318c
Rob Calcote <robcalcote@gmail.com>
2018-12-12 13:13:11 -0700

    Added DAL and built LearningDynamicsContext

---
+++ 403Project1/DAL/LearningDynamicsContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using _403Project1.Models;
+
+namespace _403Project1.DAL
+{
+    public class LearningDynamicsContext : DbContext
+    {
+        public LearningDynamicsContext() :
+            base("LearningDynamicsContext")
+        {
+
+        }
+
+        public DbSet<Parent> Parent { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<SchoolQuestion> SchoolQuestions { get; set; }
+        public DbSet<School> School { get; set; }
+    }
+}
