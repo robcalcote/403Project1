22c6f88b6ec3d4dcbd71e25e70a3bc3cf63daa11
Matt Lant <matthewclant@gmail.com>
2018-12-13 13:50:04 -0700

    all project

--- 403Project1/Controllers/HomeController.cs
+++ 403Project1/Controllers/HomeController.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using _403Project1.Models;
+using _403Project1.DAL;
+using System.Web.Security;
+using System.Data.Entity;
 
 namespace _403Project1.Controllers
 {
     public class HomeController : Controller
     {
+        private LearningDynamicsContext db = new LearningDynamicsContext();
+
         public ActionResult Index()
         {
             return View();
@@ -20,11 +26,37 @@ namespace _403Project1.Controllers
             return View();
         }
 
+        [Authorize]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(FormCollection form, [Bind(Include = "PEmail,PLogin")] Parent newparent, bool rememberMe = false)
+        {
+            string password = newparent.PLogin;
+            if (db.Parents.Find(newparent.PEmail) != null)
+            {
+                if (db.Parents.Find(newparent.PEmail).PLogin == password)
+                {
+                    FormsAuthentication.SetAuthCookie(newparent.PEmail, rememberMe);
+                    return View("Index");
+                }
+            }
+
+            return View();
+        }
+
+        
+        
     }
 }
---
+++ 403Project1/DAL/LearningDynamicsContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using _403Project1.Models;
+
+namespace _403Project1.DAL
+{
+    public class LearningDynamicsContext : DbContext
+    {
+        public LearningDynamicsContext() : base("LearningDynamicsContext")
+        {
+
+        }
+        
+        public DbSet<Parent> Parents { get; set; }
+        public DbSet<School> Schools { get; set; }
+        public DbSet<SchoolQuestion> SchoolQuestions { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
