0aaa92ae53cf4690b6fcddbc94c1cc3f69c4bd9f
Rob Calcote <robcalcote@gmail.com>
2018-12-13 17:13:34 -0700

    Created scaffolded schoolquestions along with views to answer questions.

---
+++ 403Project1/Controllers/SchoolQuestionsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using _403Project1.DAL;
+using _403Project1.Models;
+
+namespace _403Project1.Controllers
+{
+    public class SchoolQuestionsController : Controller
+    {
+        private LearningDynamicsContext db = new LearningDynamicsContext();
+
+        // GET: SchoolQuestions
+        public ActionResult Index()
+        {
+            var schoolQuestions = db.SchoolQuestions.Include(s => s.parent).Include(s => s.school);
+            return View(schoolQuestions.ToList());
+        }
+
+        // GET: SchoolQuestions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SchoolQuestion schoolQuestion = db.SchoolQuestions.Find(id);
+            if (schoolQuestion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(schoolQuestion);
+        }
+
+        // GET: SchoolQuestions/Create
+        public ActionResult Create()
+        {
+            ViewBag.ParentID = new SelectList(db.Parent, "ParentID", "PFirstName");
+            ViewBag.SchoolID = new SelectList(db.School, "SchoolID", "SchoolName");
+            return View();
+        }
+
+        // POST: SchoolQuestions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "QuestionID,SchoolID,ParentID,Question,Answer")] SchoolQuestion schoolQuestion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SchoolQuestions.Add(schoolQuestion);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ParentID = new SelectList(db.Parent, "ParentID", "PFirstName", schoolQuestion.ParentID);
+            ViewBag.SchoolID = new SelectList(db.School, "SchoolID", "SchoolName", schoolQuestion.SchoolID);
+            return View(schoolQuestion);
+        }
+
+        // GET: SchoolQuestions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SchoolQuestion schoolQuestion = db.SchoolQuestions.Find(id);
+            if (schoolQuestion == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ParentID = new SelectList(db.Parent, "ParentID", "PFirstName", schoolQuestion.ParentID);
+            ViewBag.SchoolID = new SelectList(db.School, "SchoolID", "SchoolName", schoolQuestion.SchoolID);
+            return View(schoolQuestion);
+        }
+
+        // POST: SchoolQuestions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "QuestionID,SchoolID,ParentID,Question,Answer")] SchoolQuestion schoolQuestion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(schoolQuestion).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ParentID = new SelectList(db.Parent, "ParentID", "PFirstName", schoolQuestion.ParentID);
+            ViewBag.SchoolID = new SelectList(db.School, "SchoolID", "SchoolName", schoolQuestion.SchoolID);
+            return View(schoolQuestion);
+        }
+
+        // GET: SchoolQuestions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SchoolQuestion schoolQuestion = db.SchoolQuestions.Find(id);
+            if (schoolQuestion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(schoolQuestion);
+        }
+
+        // POST: SchoolQuestions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SchoolQuestion schoolQuestion = db.SchoolQuestions.Find(id);
+            db.SchoolQuestions.Remove(schoolQuestion);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
